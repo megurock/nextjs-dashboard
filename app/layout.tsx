5673f28ea076731086d4d3626eec82fce03f076b
Eiji Meguro <eiji@fork.co.jp>
2025-05-12 14:25:29 +0900

    feat: Chapter 17 まで

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,22 @@
-import { inter } from '@/app/ui/fonts';
-import '@/app/ui/global.css';
+import { inter } from "@/app/ui/fonts";
+import "@/app/ui/global.css";
+import type { Metadata } from "next";
 
-type Props = { children: React.ReactNode }
+type Props = { children: React.ReactNode };
 
 export default function RootLayout({ children }: Props) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${inter.className} antialiased`}>{children}</body>
+		</html>
+	);
 }
+
+export const metadata: Metadata = {
+	title: {
+		template: "%s | Acme Dashboard",
+		default: "Acme Dashboard",
+	},
+	description: "The official Next.js Learn Dashboard built with App Router.",
+	metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
